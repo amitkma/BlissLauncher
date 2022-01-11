8ab986b5be6850643f8cdd69560e5367eff88eb9
Amit Kumar <amitkma@e.email>
2022-01-11 21:44:49 +0530

    Fix decrypt script

--- scripts/decrypt-secrets.sh
+++ scripts/decrypt-secrets.sh
@@ -16,20 +16,16 @@
 #  limitations under the License.
 #
 
-encrypt() {
+decrypt() {
   PASSPHRASE=$1
   INPUT=$2
   OUTPUT=$3
-  gpg --batch --yes --passphrase="$PASSPHRASE" --cipher-algo AES256 --symmetric --output $OUTPUT $INPUT
+  gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" --output "$OUTPUT" "$INPUT"
 }
 
-if [[ ! -z "$ENCRYPT_KEY" ]]; then
-  # Encrypt Release key
-  encrypt ${ENCRYPT_KEY} release/app-release.jks release/app-release.gpg
-  # Encrypt Play Store key
-  encrypt ${ENCRYPT_KEY} release/play-account.json release/play-account.gpg
-  # Encrypt Google Services key
-  encrypt ${ENCRYPT_KEY} app/google-services.json release/google-services.gpg
+if [[ -n "$ENCRYPT_KEY" ]]; then
+  # Decrypt Release key
+  decrypt "${ENCRYPT_KEY}" keystore/app-release.gpg keystore/app-release.keystore
 else
   echo "ENCRYPT_KEY is empty"
 fi
